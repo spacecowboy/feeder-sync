aa2d5ac8cd385c37acd0df6f8a44b28e5995bf86
Jonas Kalderstam <jonas@cowboyprogrammer.org>
2021-12-16 20:04:44 +0000

    Enforce max size

--- src/main.ts
+++ src/main.ts
@@ -134,6 +134,10 @@ export class SyncChain {
   sessions: Session[];
   env: unknown;
   storage: DurableObjectStorage;
+  initialization: Promise<void>;
+  readMarkKeys: string[] = [];
+  // Should be less than 1000
+  maxReadMarks = 900;
 
   constructor(state: DurableObjectState, env: unknown) {
     this.storage = state.storage;
@@ -145,9 +149,28 @@ export class SyncChain {
     // no need to store this to disk since we assume if the object is destroyed and recreated, much
     // more than a millisecond will have gone by.
     this.lastTimestamp = 0;
+
+    // Keep all read marks in memory. This way it is easy to implement FIFO
+    this.initialization = this._initialize();
+  }
+
+  async _initialize(): Promise<void> {
+    const stuff = await this.storage.list({
+      prefix: "R_",
+    });
+
+    this.readMarkKeys = [...stuff.keys()];
+
+    if (this.readMarkKeys.length > this.maxReadMarks) {
+      await this.storage.delete(this.readMarkKeys.slice(0, -this.maxReadMarks));
+      this.readMarkKeys = this.readMarkKeys.slice(-this.maxReadMarks);
+    }
   }
 
   async fetch(request: Request): Promise<Response> {
+    // This only waits first time and all flows enter through here
+    await this.initialization;
+
     return await handleErrors(request, async () => {
       const url = new URL(request.url);
 
@@ -170,143 +193,20 @@ export class SyncChain {
               return new Response(null, { status: 405 });
           }
         }
-        //   case "/websocket": {
-        //     // A client is trying to establish a new WebSocket session.
-        //     if (request.headers.get("Upgrade") != "websocket") {
-        //       return new Response("expected websocket upgrade", { status: 400 });
-        //     }
-
-        //     // Get the client's IP address for use with the rate limiter.
-        //     const ip = request.headers.get("CF-Connecting-IP");
-
-        //     // To accept the WebSocket request, we create a WebSocketPair (which is like a socketpair,
-        //     // i.e. two WebSockets that talk to each other), we return one end of the pair in the
-        //     // response, and we operate on the other end. Note that this API is not part of the
-        //     // Fetch API standard; unfortunately, the Fetch API / Service Workers specs do not define
-        //     // any way to act as a WebSocket server today.
-        //     const pair = new WebSocketPair();
-
-        //     // We're going to take pair[1] as our end, and return pair[0] to the client.
-        //     await this.handleSession(pair[1], ip);
-
-        //     // Now we return the other end of the pair to the client.
-        //     return new Response(null, { status: 101, webSocket: pair[0] });
-        //   }
         default:
           return new Response(`Not found: ${url.pathname}`, { status: 404 });
       }
     });
   }
 
-  async handleSession(webSocket: WebSocket, ip: string | null): Promise<void> {
-    // Accept our end of the WebSocket. This tells the runtime that we'll be terminating the
-    // WebSocket in JavaScript, not sending it elsewhere.
-    webSocket.accept();
-
-    // TODO rate limiter
-
-    const session = {
-      webSocket,
-      dead: false,
-    };
-    this.sessions.push(session);
-
-    // On "close" and "error" events, remove the WebSocket from the sessions list
-    const closeOrErrorHandler = (msg: Event) => {
-      session.dead = true;
-      this.sessions = this.sessions.filter((member) => member !== session);
-    };
-    webSocket.addEventListener("close", closeOrErrorHandler);
-    webSocket.addEventListener("error", closeOrErrorHandler);
-    webSocket.addEventListener("message", async (msg: MessageEvent) => {
-      try {
-        if (session.dead) {
-          // We received a message but marked the session as dead - should never happen but hey
-          webSocket.close(1011, "WebSocket broken.");
-          return;
-        }
-
-        // TODO check rate limit
-        let data;
-
-        if (typeof msg.data === "string") {
-          data = JSON.parse(msg.data);
-        } else {
-          webSocket.send(
-            JSON.stringify({
-              type: "E",
-              error: "message data was not string",
-            })
-          );
-          return;
-        }
-
-        switch (data.type) {
-          case "R":
-            await this.markAsRead(data, session);
-            return;
-          case "G":
-            await this.getRead(data, session);
-            return;
-          default:
-            webSocket.send(
-              JSON.stringify({
-                type: "E",
-                error: "Unknown type: " + data.type,
-              })
-            );
-            return;
-        }
-      } catch (err) {
-        // Report any exceptions directly back to the client. As with our handleErrors() this
-        // probably isn't what you'd want to do in production, but it's convenient when testing.
-        // @ts-ignore
-        webSocket.send(JSON.stringify({ type: "E", error: err.stack }));
-      }
-    });
-  }
-
-  async markAsRead(data: ReadMarkMessage, session: Session): Promise<void> {
-    // Add timestamp. Here's where this.lastTimestamp comes in -- if we receive a bunch of
-    // messages at the same time (or if the clock somehow goes backwards????), we'll assign
-    // them sequential timestamps, so at least the ordering is maintained.
-    this.lastTimestamp = Math.max(Date.now(), this.lastTimestamp + 1);
-    data.timestamp = this.lastTimestamp;
-    const dataStr = JSON.stringify(data);
-
-    // Save message.
-    const suffix = data.timestamp.toString();
-    const key = `R_${suffix}`;
-    await this.storage.put(key, dataStr);
-
-    // TODO send latest timestamp to self
-
-    // Broadcast the message to all other WebSockets.
-    this.broadcast(dataStr, session);
-  }
-
-  broadcast(data: string, senderSession: Session): void {
-    // Update sessions list in case any of the sessions are dead
-    this.sessions = this.sessions.filter((session) => {
-      // Don't send to yourself
-      if (session === senderSession) {
-        return true;
-      }
-      try {
-        session.webSocket.send(data);
-        return true;
-      } catch (err) {
-        // Whoops, this connection is dead. Mark it as such and remove it
-        session.dead = true;
-        return false;
-      }
-    });
-  }
-
   async markAsReadRest(data: SendReadMarkBulkRequest): Promise<Response> {
+    if (data.items.length > this.maxReadMarks) {
+      return new Response("U 2 phat", { status: 400 });
+    }
+
     for (const mark of data.items) {
       this.lastTimestamp = Math.max(Date.now(), this.lastTimestamp + 1);
-      const readMark = {
+      const readMark: ReadMarkMessage = {
         timestamp: this.lastTimestamp,
         feedUrl: mark.feedUrl,
         articleGuid: mark.articleGuid,
@@ -315,6 +215,13 @@ export class SyncChain {
       const suffix = readMark.timestamp;
       const key = `R_${suffix}`;
       await this.storage.put(key, JSON.stringify(readMark));
+      // And in-memory collection of keys
+      this.readMarkKeys.push(key);
+    }
+
+    if (this.readMarkKeys.length > this.maxReadMarks) {
+      await this.storage.delete(this.readMarkKeys.slice(0, -this.maxReadMarks));
+      this.readMarkKeys = this.readMarkKeys.slice(-this.maxReadMarks);
     }
 
     return new Response(JSON.stringify({ timestamp: this.lastTimestamp }), {
@@ -323,8 +230,7 @@ export class SyncChain {
   }
 
   async getReadRest(since: number): Promise<Response> {
-    // TODO pagination
-
+    // No need for pagination - it's sorta built in to the entire since parameter
     const storage = await this.storage.list({
       prefix: "R_",
       start: `R_${since}`,
@@ -347,34 +253,6 @@ export class SyncChain {
     return new Response(JSON.stringify({ readMarks: marks }), {
       status: 200,
       headers: headers,
-    });
-  }
-
-  async getRead(data: GetReadMessage, session: Session): Promise<void> {
-    const since = data.since;
-    // TODO pagination
-    /*
-
-interface DurableObjectListOptions {
-  start?: string;
-  end?: string;
-  prefix?: string;
-  reverse?: boolean;
-  limit?: number;
-  allowConcurrency?: boolean;
-  noCache?: boolean;
-}
-    */
-    const storage = await this.storage.list({
-      prefix: "R_",
-      start: `R_${since.toString()}`,
-    });
-    const values = [...storage.values()];
-
-    values.forEach((value) => {
-      if (typeof value === "string") {
-        session.webSocket.send(value);
-      }
     });
   }
 }
