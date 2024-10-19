e00683879e2c3d25010ea668c3fbbc131a19ff9f
Jonas Kalderstam <spacecowboy@users.noreply.github.com>
2024-10-19 22:50:44 +0200

    fix: add graceful shutdown (#3)

--- cmd/webserver/main.go
+++ cmd/webserver/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/spacecowboy/feeder-sync/internal/server"
 )
@@ -15,18 +19,43 @@ func main() {
 		log.Fatal("FEEDER_SYNC_POSTGRES_CONN environment variable not set")
 	}
 
-	server, err := server.NewServerWithPostgres(conn)
-
+	router, err := server.NewServerWithPostgres(conn)
 	if err != nil {
-		log.Fatalf("Failed to create server: %q", err)
+		log.Fatalf("Failed to create server: %v", err)
 	}
-	defer func() {
-		if err := server.Close(); err != nil {
-			return
+	defer router.Close()
+
+	srv := &http.Server{
+		Addr:    ":34217",
+		Handler: router,
+	}
+
+	// Initializing the server in a goroutine so that
+	// it won't block the graceful shutdown handling below
+	go func() {
+		log.Printf("Serving on %q...", srv.Addr)
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Failed to serve: %v", err)
 		}
 	}()
 
-	addr := ":34217"
-	log.Printf("Serving on %q", addr)
-	log.Fatal(http.ListenAndServe(addr, server))
+	// Wait for interrupt signal to gracefully shutdown the server with
+	// a timeout of 5 seconds.
+	quit := make(chan os.Signal, 1)
+	// kill (no param) default send syscall.SIGTERM
+	// kill -2 is syscall.SIGINT
+	// kill -9 is syscall.SIGKILL but can't be catch, so don't need add it
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutting down server...")
+
+	// The context is used to inform the server it has 5 seconds to finish
+	// the request it is currently handling
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("Server forced to shutdown: %v", err)
+	}
+
+	log.Println("Server exiting")
 }
